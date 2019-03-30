9ce5506db33542d76710726b417d540df99ae937
Marian Bahniuk <marbug@faqdev.com>
2019-03-30 18:52:55 +0200

    enhance scripts/git-save-repo-to-faqdev.sh

--- scripts/git-save-repo-to-faqdev.sh
+++ scripts/git-save-repo-to-faqdev.sh
@@ -2,13 +2,22 @@
 
 scriptName=$0
 scriptDir=`dirname ${scriptName}`
+echo "==== Script dir"
 echo ${scriptDir}
+cd ${scriptDir}/..
 
+echo "==== Current branch ..."
+currentBranch=`git branch | grep '^*' | awk '{print $2;}'`
+echo ${currentBranch}
+
+echo "==== Save to BitBucket ..."
 ${scriptDir}/git-use-faqdev-bitbucket.sh
-git push -f
+git push -f --set-upstream origin ${currentBranch}
 
+echo "==== Save to GitHub ..."
 ${scriptDir}/git-use-faqdev-github.sh
-git push -f
+git push -f --set-upstream origin ${currentBranch}
 
+echo "==== Save to GitLab ..."
 ${scriptDir}/git-use-faqdev-gitlab.sh
-git push -f
+git push -f --set-upstream origin ${currentBranch}
