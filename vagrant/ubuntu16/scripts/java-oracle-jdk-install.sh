c99034c27546ebf0d0bb805c23b2d15154bf96d4
Marian Bahniuk <marbug@faqdev.com>
2019-04-03 19:48:22 +0300

    add vagrant/ubuntu16/scripts/java-oracle-jdk-install.sh

---
+++ vagrant/ubuntu16/scripts/java-oracle-jdk-install.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+numberOfJdkFiles=`ls -1 /vagrant/downloads/jdk*.deb | wc -l`
+echo "==== number of JDK files"
+echo ${numberOfJdkFiles}
+
+if [ "${numberOfJdkFiles}" != "1" ]; then
+    echo "Please leave only one JDK file in /vagrant/downloads folder"
+    exit 1
+fi
+
+numberOfInstalledJdks=`find /usr/lib/jvm -name 'javac' | wc -l`
+echo "==== number of installed JDKs"
+echo ${numberOfInstalledJdks}
+
+if [ "${numberOfInstalledJdks}" != "0" ]; then
+    echo "The ${numberOfInstalledJdks} installed JDK(s) found. Please remove."
+    exit 1
+fi
+
+lastFileName=`ls -1 /vagrant/downloads/jdk*.deb`
+echo "==== JDK file name"
+echo "${lastFileName}"
+
+echo "==== install ${lastFileName}"
+sudo apt-get install -y ${lastFileName}
+
+echo "==== get java directory"
+javacPath=`find /usr/lib/jvm -name 'javac'`
+javacPath=`dirname ${javacPath}`
+echo ${javacPath}
+
+echo "==== go to /home/vagrant"
+cd /home/vagrant
+
+echo "==== backup"
+envFilePath="./environment"
+. /etc/environment
+echo -n 'PATH="' > ${envFilePath}
+echo -n $PATH >> ${envFilePath}
+echo -n ':' >> ${envFilePath}
+echo -n ${javacPath} >> ${envFilePath}
+echo '"' >> ${envFilePath}
+cat ${envFilePath}
+sudo cp ${envFilePath} /etc/environment
