501bc663e9fa9a6e187706a8906809bcf25f1984
Marian Bahniuk <marbug@faqdev.com>
2019-04-05 18:26:18 +0000

    add laravel install scripts

---
+++ vagrant/ubuntu16/scripts/php-v7.2-install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "==== Install PHP"
+sudo apt-get update && sudo apt-get -y dist-upgrade
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:ondrej/php
+sudo add-apt-repository -y ppa:ondrej/apache2
+sudo add-apt-repository -y ppa:ondrej/nginx
+sudo apt-get update && sudo apt-get -y --allow-unauthenticated dist-upgrade
+sudo apt-get install -y --allow-unauthenticated php7.2
+sudo apt-get update && sudo apt-get -y --allow-unauthenticated dist-upgrade
+
+sudo apt-get install -y --allow-unauthenticated php7.2-zip
+sudo apt-get install -y --allow-unauthenticated php7.2-mbstring
+sudo apt-get install -y --allow-unauthenticated php7.2-dom
+
+# MySQL support:
+# sudo apt-get install -y --allow-unauthenticated php7.2-mysql
